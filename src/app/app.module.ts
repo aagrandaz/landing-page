2ab9108469d7321ae76e4d7d1cecacbecc1693c7
Abel Granda <abelgranda@hotmail.es>
2023-11-06 19:38:53 -0500

    Segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NombreComponent } from './nombre/nombre.component';
+import { InicioComponent } from './inicio/inicio.component';
 
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'nombre', component: NombreComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NombreComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
